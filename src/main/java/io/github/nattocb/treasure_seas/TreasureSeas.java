24f7302095492d61575340535ae832b78b077556
NattoCB <jasper.fang@qq.com>
2024-08-27 16:07:35 +0800

    manage todos

--- src/main/java/io/github/nattocb/treasure_seas/TreasureSeas.java
+++ src/main/java/io/github/nattocb/treasure_seas/TreasureSeas.java
@@ -46,6 +46,8 @@ public class TreasureSeas {
                 () -> ClientProxy::new,
                 () -> CommonProxy::new
         );
+        // 注册日志管理
+        logManager = new LogManager(LoggerFactory.getLogger(TreasureSeas.class));
         // 方块、物品、附魔、容器注册
         DeferredRegister<Enchantment> enchantments = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, TreasureSeas.MOD_ID);
         enchantments.register(FMLJavaModLoadingContext.get().getModEventBus());
@@ -55,8 +57,6 @@ public class TreasureSeas {
         ModBlocks.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ModBlocks.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        // 注册日志 handler
-        logManager = new LogManager(LoggerFactory.getLogger(TreasureSeas.class));
     }
 
     @SubscribeEvent
@@ -82,11 +82,10 @@ public class TreasureSeas {
     // todo 附魔等级越高 loot 该附魔书几率越低
     // todo 低等级附魔钓不到某个百分比阈值以上的长度的鱼（是否需要加？）
     // todo 统计水域面积，显示过小适中广阔，过小时只能掉到垃圾，但避免计算无限大，最大不要超过 16 * 16，过大则 tootip 显示（广阔）
-    // todo 客户端配置在每次进入游戏时重载
     // todo 不同的稀有度会越来越难钓（对于低等级钓竿来说）
     // todo 甩竿决定深度，新 gui
-    // todo 如果 world path not found，默认用 overworld（但前提是，世界类型需要是 overworld）从而支持 multiverse 的其他主世界
-    // todo mod logo png in toml file
+    // todo 增加暮色默认鱼
+    // todo 增加海灵物语默认鱼
     // todo 个人计分板（服务器排行榜？如何联动）
-    // todo 写 readme
+
 }
